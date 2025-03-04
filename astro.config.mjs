03f1f0e3cc99dab8dc72c76f363bdb6bfd62af69
unitedlyco <don@unitedly.co>
2025-03-04 22:40:34 +0800

    Fix rendering issues by enabling prerendering for static builds and configuring Vercel adapter properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://foxi.netlify.app/",
+  output: process.env.NODE_ENV === "production" ? "server" : "static",
+  adapter: process.env.NODE_ENV === "production" ? vercel() : undefined,
   integrations: [
     tailwind(),
     icon(),
